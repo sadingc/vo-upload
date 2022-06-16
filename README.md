269ceb8133a55f9f741455a1ac74a79a69658186
sadingc <103971417+sadingc@users.noreply.github.com>
2022-06-16 00:51:11 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+### Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/richardcuba1115/draft_to_calendar)
---
+++ conf.py
@@ -0,0 +1,4 @@
+api_hash = "a86730aab5c59953c424abb4396d32d5"
+api_id = 14681595
+bot_token = "5232913510:AAEwPW72m2fPJ_sYhg29eDPTbnxNrvRZWi0"
+bot_admin_group = -1001689722383
